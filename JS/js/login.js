b805a83c109343067f6ff233a272d9d760532d96
deMelo1 <130903363+deMelo1@users.noreply.github.com>
2023-05-20 20:37:22 -0300

    Update login.js

--- JS/js/login.js
+++ JS/js/login.js
@@ -5,6 +5,7 @@ const tipo = document.getElementById('tipo')
 const senhaerrada = document.getElementById('senhaerrada')
 let testeString;
 let testeCerto;
+senhaerrada.textContent = ''
 login.addEventListener("submit", event => {
     event.preventDefault();
     for (let i in localStorage){
@@ -17,6 +18,7 @@ login.addEventListener("submit", event => {
                 console.log("CERTO")
                 email.setAttribute("style", "border: 2px solid green");
                 senha.setAttribute("style", "border: 2px solid green");
+                senhaerrada.textContent = ''
                 break
             }
             else{
@@ -24,8 +26,15 @@ login.addEventListener("submit", event => {
                 email.setAttribute("style", "border: 2px solid red");
                 senha.setAttribute("style", "border: 2px solid red");
                 console.log(senhaerrada.value)
-                if (email.vale == testeCerto.emailc){
+                if (email.value == testeCerto.emailc){
+                    if(senha.value == testeCerto.senhac){
+                        senhaerrada.textContent = ''
+                        senhaerrada.textContent = 'Nível de acesso incorreto, tente novamente!'
+                        break
+                    }
+                    senhaerrada.textContent = ''
                     senhaerrada.textContent = 'Senha incorreta, tente novamente!'
+                    break
                 }
                 else{
                     senhaerrada.textContent = 'Usuário não cadastrado, clique em cadastre-se para se cadastrar!'
